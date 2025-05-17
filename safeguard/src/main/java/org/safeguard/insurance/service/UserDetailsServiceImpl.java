db49ff03d2be995f54d4ff1c2683f9095a970463
mahes <mahes@DESKTOP-2VM4CIO>
2025-05-17 22:07:25 +0530

    test

--- safeguard/src/main/java/org/safeguard/insurance/service/UserDetailsServiceImpl.java
+++ safeguard/src/main/java/org/safeguard/insurance/service/UserDetailsServiceImpl.java
@@ -4,25 +4,25 @@ import org.safeguard.insurance.domainmodel.CustomUserDetails;
 import org.safeguard.insurance.entitymodel.User;
 import org.safeguard.insurance.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    @Autowired
-    private UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username);
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username);
 
-        if (user == null) {
-            throw new UsernameNotFoundException("User not found");
-        }
+		if (user == null) {
+			throw new UsernameNotFoundException("User not found");
+		}
 
-        return new CustomUserDetails(user.getUsername(), user.getPassword());
-    }
+		return new CustomUserDetails(user.getUsername(), user.getPassword());
+	}
 }
